96a93aba41cdb24a993920c6c9cfe8094e173284
yeonwoo <mamen7624@gmail.com>
2024-06-01 17:06:20 +0900

    Initial Commit

---
+++ tableViewEx1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewEx1
+//
+//  Created by 여누 on 6/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
